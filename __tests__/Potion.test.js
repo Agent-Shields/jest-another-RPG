d55756df9b9e5748d0a33470636532d0df618250
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-01-27 18:36:37 -0600

    code snapshot finished lesson 10.4

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,4 +1,3 @@
-const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
@@ -15,16 +14,3 @@ test('creates a random potion object', () => {
   expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });
-
-test("subtracts from player's health", () => {
-  const player = new Player('Dave');
-  const oldHealth = player.health;
-
-  player.reduceHealth(5);
-
-  expect(player.health).toBe(oldHealth - 5);
-
-  player.reduceHealth(99999);
-
-  expect(player.health).toBe(0);
-})
